3846331d37e235330c01c39d1279af4f8540880d
job12 <dasdwasd@gmail.com>
2022-10-18 14:24:38 +0700

    first commit

---
+++ app/src/main/java/com/app/job12/activity_second.kt
@@ -0,0 +1,11 @@
+package com.app.job12
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_second : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
